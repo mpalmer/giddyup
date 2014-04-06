7b5f74024e26357d636c586e75a40d8ddff78ecf
Matt Palmer <mpalmer@hezmatt.org>
2014-04-06 18:29:32 +1000

    New giddyup type option: origin
    Set the repo origin to the specified URL.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,18 +1,21 @@
-define giddyup($environment = undef,
-               $hookdir = undef,
-               $keepreleases = undef,
-               $debug = undef,
-               $user) {
+define giddyup(
+	$environment  = undef,
+	$hookdir      = undef,
+	$keepreleases = undef,
+	$debug        = undef,
+	$origin       = undef,
+	$user
+) {
 	include giddyup::install
 	include git::packages
-	
+
 	exec { "giddyup create ${name}":
 		command => "/usr/local/lib/giddyup/giddyup ${name}",
 		creates => $name,
 		user => $user,
 		require => [ File["/usr/local/lib/giddyup/giddyup"], User[$user] ]
 	}
-	
+
 	giddyup::config {
 		"giddyup.environment for ${name}":
 			base => $name,
@@ -34,5 +37,15 @@ define giddyup($environment = undef,
 			user => $user,
 			var => "debug",
 			value => $debug;
+	}
+
+	if $origin {
+		exec { "set remote.origin.url in ${name}":
+			command => "/usr/bin/git config -f '${name}/repo/config' 'remote.origin.url' '${origin}'",
+			unless => "/usr/bin/test \"\$(git config -f ${name}/repo/config remote.origin.url)\" = '${origin}'",
+			cwd => "/",
+			user => $user,
+			require => [ Noop["git/installed"], Exec["giddyup create ${name}"], User[$user] ]
+		}
 	}
 }
