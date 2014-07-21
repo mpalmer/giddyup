ac24b749120e5313f90881ce214a288d9ffef3d2
Matt Palmer <mpalmer@hezmatt.org>
2014-07-21 12:21:33 +1000

    I want *files*, damnit

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,21 +1,24 @@
 class giddyup::install {
 	file {
 		"/usr/local/lib/giddyup":
-			ensure => directory,
-			mode => 0755;
+			ensure  => directory,
+			mode    => 0755;
 		"/usr/local/lib/giddyup/giddyup":
-			source => "puppet:///modules/giddyup/giddyup",
-			mode => 0555,
+			ensure  => file,
+			source  => "puppet:///modules/giddyup/giddyup",
+			mode    => 0555,
 			require => [ File["/usr/local/lib/giddyup/update-hook"],
 			             File["/usr/local/lib/giddyup/functions.sh"]
 			           ];
 		"/usr/local/lib/giddyup/update-hook":
-			source => "puppet:///modules/giddyup/update-hook",
-			mode => 0555;
+			ensure  => file,
+			source  => "puppet:///modules/giddyup/update-hook",
+			mode    => 0555;
 		"/usr/local/lib/giddyup/functions.sh":
-			source => "puppet:///modules/giddyup/functions.sh",
-			mode => 0444;
+			ensure  => file,
+			source  => "puppet:///modules/giddyup/functions.sh",
+			mode    => 0444;
 		"/usr/local/bin/giddyup":
-			ensure => "/usr/local/lib/giddyup/giddyup";
+			ensure  => "/usr/local/lib/giddyup/giddyup";
 	}
 }
