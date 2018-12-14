cc0c1ff772c1612027e4057e89b960db62d2be88
Matt Palmer <mpalmer@hezmatt.org>
2018-12-14 13:32:21 +1100

    First round of future parser support

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,12 +2,12 @@ class giddyup::install {
 	file {
 		"/usr/local/lib/giddyup":
 			ensure  => directory,
-			mode    => 0755;
+			mode    => "0755";
 		"/usr/local/lib/giddyup/giddyup":
 			ensure  => file,
 			links   => follow,
 			source  => "puppet:///modules/giddyup/giddyup",
-			mode    => 0555,
+			mode    => "0555",
 			require => [ File["/usr/local/lib/giddyup/update-hook"],
 			             File["/usr/local/lib/giddyup/functions.sh"]
 			           ];
@@ -15,12 +15,12 @@ class giddyup::install {
 			ensure  => file,
 			links   => follow,
 			source  => "puppet:///modules/giddyup/update-hook",
-			mode    => 0555;
+			mode    => "0555";
 		"/usr/local/lib/giddyup/functions.sh":
 			ensure  => file,
 			links   => follow,
 			source  => "puppet:///modules/giddyup/functions.sh",
-			mode    => 0444;
+			mode    => "0444";
 		"/usr/local/bin/giddyup":
 			ensure  => "/usr/local/lib/giddyup/giddyup";
 	}
